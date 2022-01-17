e87ea629220f99fa8fd5b6065c670605197f4fed
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-01-17 13:55:29 -0300

    :sparkles: Add error pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root to: 'posts#index'
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
