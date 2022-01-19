5752bd8f9fba04be9f163b68bd2e7cba7b1e484a
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-01-19 13:43:46 -0300

    :ambulance: Fix temporarily post creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_124225) do
+ActiveRecord::Schema.define(version: 2022_01_19_123626) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -55,8 +55,6 @@ ActiveRecord::Schema.define(version: 2022_01_19_124225) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -80,5 +78,4 @@ ActiveRecord::Schema.define(version: 2022_01_19_124225) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "posts", "users"
 end
