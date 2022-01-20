c9ed083caeb5c071d012fcc0b91c0cdacb1573f3
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-01-20 15:50:52 -0300

    :construction: Limit registrations and change some language

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }, inclusion: { in: %w[gabrielogomes@live.com]}
 end
