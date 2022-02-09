e0c1fb1827fad00eaad4e6542e96dc23bc19c552
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-02-09 19:23:16 -0300

    :sparkles: Add a gambiarra to fix prod first page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,23 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.order(created_at: :asc).last(7)
+    check_content
+  end
+
+  private
+
+  def total_posts
+    Post.count > 7 ? 7 : Post.count
+  end
+
+  def check_content
+    @posts = Post.order(created_at: :desc).last(6)
+
+    if @posts.length < 7
+      i = @posts.length
+      while i <= 7
+        @posts[i] = @posts[0]
+        i += 1
+      end
+    end
   end
 end
