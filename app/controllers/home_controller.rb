2d7b3ff969ee02486b2c532ee3e5260bbd5f6347
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-02-26 16:40:57 -0300

    :lipstick: Add footer styling and change homepage styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.order("created_at desc").page(params[:page])
-    @tags = Tag.order("taggings_count asc").last(10)
   end
 end
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,4 +2,8 @@ module TagsHelper
   def find_tag_by_name(tag)
     Tag.find_by_name(tag).id
   end
+
+  def most_used_tags
+    Tag.order("taggings_count asc").last(10)
+  end
 end
